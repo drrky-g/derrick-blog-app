3eb70db1f8ad9f0bba3ee865bffe0daaa59a442e
Derrick Gordon <derrickwg17@gmail.com>
2019-07-21 23:29:33 -0400

    style tweaks
    some small style tweaks to personalize the page

--- derrick-blog-app/App_Start/RouteConfig.cs
+++ derrick-blog-app/App_Start/RouteConfig.cs
@@ -33,7 +33,7 @@ namespace derrick_blog_app
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "BlogPosts", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
